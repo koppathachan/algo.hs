3c34dcbd3d65749399de735a820fefce55791d9d
sasidakh <akhilsasidharan1991@gmail.com>
2020-05-17 21:23:03 +0530

    update function in a single function

--- getPointsOnCircle.hs
+++ getPointsOnCircle.hs
@@ -1,15 +1,4 @@
-module Geometry
-
-tupleList :: Int -> [(Int, Int)]
-tupleList n = [(x,x) | x <- [1..n]]
-
-thetas :: [(Int, Int)] -> [(Double, Double)]
-thetas l = [(2 * pi / fromIntegral x, 2 * pi / fromIntegral y) | (x,y) <- l]
-
-pxy :: Double -> [(Double, Double)] -> [(Double, Double)]
-pxy r l = [(r * (cos x), r * (sin y)) | (x, y) <- l]
-
-adjCenter ::(Double, Double) ->  [(Double, Double)] -> [(Double, Double)]
-adjCenter c l = [(fst c + x, snd c + y) | (x, y) <- l]
-
-getNPointsOnCircle r n c = adjCenter c (pxy r (thetas(tupleList(n))))
+nPtsOnCircle :: Double -> Int -> (Double, Double) -> [(Double, Double)] 
+nPtsOnCircle r n c = 
+    [(fst c + (r * (cos (fromIntegral x * (pi / 2)))), snd c + (r * (sin (fromIntegral x * (pi / 2))))) 
+        | x <- [1..n]]
